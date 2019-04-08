aad991f0e56c0c4616c2d50d4fc0c557217c4b40
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-08 18:29:00 +0200

    05 ok

---
+++ lib/05_timer.rb
@@ -0,0 +1,7 @@
+def time_string(total_seconds)
+	seconds = total_seconds % 60
+minutes = (total_seconds / 60) % 60
+hours = total_seconds / (60 * 60)
+
+format("%02d:%02d:%02d", hours, minutes, seconds)
+end
